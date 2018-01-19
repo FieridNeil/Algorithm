e32c6f4aa565863f6e6b32861d80233d4e6ce579
Fierid Neil <14002607+FieridNeil@users.noreply.github.com>
2018-01-18 23:20:07 -0800

    Update main.cpp

--- Sort/main.cpp
+++ Sort/main.cpp
@@ -4,17 +4,37 @@
 using namespace std;
 
 int main() {
+	cout << "Selection Sort" << endl;
 	int a[10] = { 35,57,12,20,15,61,3,63,46,10 };
 	for (int i = 0; i < 10; i++) {
 		cout << a[i] << " ";
 	}
 	cout << endl;
+
+	
 	SelectionSort(a, 10, false);
 
 	for (int i = 0; i < 10; i++) {
 		cout << a[i] << " ";
 	}
 	cout << endl;
+
+	cout << "Bubble Sort" << endl;
+	int b[10] = { 35,57,12,20,15,61,3,63,46,10 };
+	for (int i = 0; i < 10; i++) {
+		cout << b[i] << " ";
+	}
+	cout << endl;
+
+	BubbleSort(b, 10, false);
+
+	for (int i = 0; i < 10; i++) {
+		cout << b[i] << " ";
+	}
+	cout << endl;
+
+
+
 
 	return 0;
 }
